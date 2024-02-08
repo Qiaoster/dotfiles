f05ee7200658b92cfe22a6f946b79d1216028eac
qiaoster <zimuzhang1@gmail.com>
2024-02-08 23:39:24 +0800

    shouldn't add perhost files

--- .config/doom/perhost.el
+++
@@ -1,4 +0,0 @@
-(visual-line-mode t)
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 14))
-                                        ;
