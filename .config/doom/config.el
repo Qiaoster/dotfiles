acbc2acf243fd696cf2222f3af1f2adecfb4d4e5
qiaoster <zimuzhang1@gmail.com>
2024-07-26 09:20:35 +0800

    emacs wl clipboard intergration

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -84,6 +84,22 @@
   (setq org-clock-persist t)
   (org-clock-persistence-insinuate))
 
+(setq wl-copy-process nil)
+(defun wl-copy (text)
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                      :buffer nil
+                                      :command '("wl-copy" "-f" "-n")
+                                      :connection-type 'pipe
+                                      :noquery t))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+(defun wl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil
+    (shell-command-to-string "wl-paste -n | tr -d \r")))
+(setq interprogram-cut-function 'wl-copy)
+(setq interprogram-paste-function 'wl-paste)
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
