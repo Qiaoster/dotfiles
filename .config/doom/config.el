b666144a39c6e08c3cea73388011a5ac1b5b365c
qiaoster <zimuzhang1@gmail.com>
2024-11-18 02:04:37 +0800

    s

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -20,8 +20,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 18 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 18))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 16))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
