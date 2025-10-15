ae47b70b69ec69276f21f9f2e2f3911aaad86d10
Qiaoster <zimuzhang1@gmail.com>
2025-10-16 00:24:37 +0800

    fairyfloss

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,6 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
+ '(custom-safe-themes
+   '("b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)")))
 (custom-set-faces
